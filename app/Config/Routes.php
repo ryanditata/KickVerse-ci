656399562b744e7c924cbdffc9593b4b7c3d6f0c
Elenanda <ellenanda456@gmail.com>
2025-07-06 22:10:34 +0700

    CRUD Library

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -12,8 +12,18 @@ $routes->get('login', 'AuthController::login');
 $routes->post('login', 'AuthController::login');
 $routes->get('logout', 'AuthController::logout');
  
-$routes->get('produk', 'ProdukController::index', ['filter' => 'auth']);
+$routes->group('produk', ['filter' => 'auth'], function ($routes) { 
+    $routes->get('', 'ProdukController::index');
+    $routes->post('', 'ProdukController::create');
+    $routes->post('edit/(:any)', 'ProdukController::edit/$1');
+    $routes->get('delete/(:any)', 'ProdukController::delete/$1');
+    $routes->get('download', 'ProdukController::download');
+});
 
-$routes->get('keranjang', 'TransaksiController::index', ['filter' => 'auth']);
-
-$routes->get('profile', 'Home::profile', ['filter' => 'auth']);
+$routes->group('keranjang', ['filter' => 'auth'], function ($routes) {
+    $routes->get('', 'TransaksiController::index');
+    $routes->post('', 'TransaksiController::cart_add');
+    $routes->post('edit', 'TransaksiController::cart_edit');
+    $routes->get('delete/(:any)', 'TransaksiController::cart_delete/$1');
+    $routes->get('clear', 'TransaksiController::cart_clear');
+});
